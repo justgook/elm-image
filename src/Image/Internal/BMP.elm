377c7e6bcdc37a665b69f241ca69f3aa9ff94639
Folkert <folkert@folkertdev.nl>
2019-11-03 16:49:26 +0100

    70% speed increase for bmp encoding
    still one failing test for rgb

--- src/Image/Internal/BMP.elm
+++ src/Image/Internal/BMP.elm
@@ -88,13 +88,10 @@ encode imageData =
         bytesPerPixel =
             bytesPerPixel_ format
 
-        size_ w =
-            w * bytesPerPixel
-
-        pad_ w =
+        padWidth w =
             let
                 improveMe =
-                    and (4 - and (size_ w) bytesPerPixel) bytesPerPixel
+                    and (4 - and (w * bytesPerPixel) bytesPerPixel) bytesPerPixel
             in
             if improveMe == 4 then
                 0
@@ -102,102 +99,101 @@ encode imageData =
             else
                 improveMe
 
-        pad__ w =
-            unsignedInt8 0
-                |> List.repeat (pad_ w)
-
-        intToBytes_ =
-            intToBytes bytesPerPixel
-
         width =
             ImageData.width_ imageData
 
         data =
             ImageData.toList2d imageData
-    in
-    (List.foldl
-        (\row ( height, acc ) ->
-            let
-                noLimit =
-                    (<) width
 
-                ( pxCount_, encodedRow_ ) =
-                    encodeRow noLimit intToBytes_ row 0 []
+        orderRight =
+            order == RightDown || order == RightUp
 
-                padding =
-                    width - pxCount_
+        orderUp =
+            order == RightUp || order == LeftUp
 
-                ( rowWidth, encodedRow ) =
-                    if padding > 0 then
-                        encodeRow noLimit
-                            intToBytes_
-                            (List.repeat padding defaultColor)
-                            pxCount_
-                            encodedRow_
+        encodeFolder remaining height totalBytes acc =
+            case remaining of
+                row :: rest ->
+                    let
+                        ( pxCount_, encodedRow_ ) =
+                            encodeRow (intToBytes bytesPerPixel) row 0 []
+
+                        -- if this row has fewer pixels than the width, recalculate
+                        ( rowWidth, encodedRow ) =
+                            let
+                                padding =
+                                    width - pxCount_
+                            in
+                            if padding > 0 then
+                                encodeRow
+                                    (intToBytes bytesPerPixel)
+                                    (List.repeat padding defaultColor)
+                                    pxCount_
+                                    encodedRow_
+
+                            else
+                                ( pxCount_, encodedRow_ )
+
+                        extra =
+                            padWidth rowWidth
+
+                        withPaddedBytes =
+                            if extra == 0 then
+                                acc
+
+                            else
+                                E.sequence (List.repeat extra (E.unsignedInt8 0)) :: acc
+
+                        withRow =
+                            if orderRight then
+                                E.sequence (List.reverse encodedRow) :: withPaddedBytes
+
+                            else
+                                E.sequence encodedRow :: withPaddedBytes
+
+                        _ =
+                            Debug.log "pixels" ( pxCount_, rowWidth, padWidth rowWidth )
+                    in
+                    encodeFolder rest (height + 1) (totalBytes + rowWidth * bytesPerPixel + extra) withRow
+
+                [] ->
+                    let
+                        body =
+                            if orderUp then
+                                List.reverse acc
+
+                            else
+                                acc
+
+                        _ =
+                            Debug.log "widths" ( totalBytes, body |> E.sequence |> E.encode |> Bytes.width )
+                    in
+                    if format == RGBA then
+                        header32 width height totalBytes body
 
                     else
-                        ( pxCount_, encodedRow_ )
+                        header16_24 (8 * bytesPerPixel) width height totalBytes body
+    in
+    encodeFolder data 0 0 []
+        |> E.sequence
+        |> E.encode
 
-                withPaddedBytes =
-                    E.sequence
-                        [ E.sequence (applyIf (order == RightDown || order == RightUp) List.reverse encodedRow)
-                        , E.sequence (pad__ rowWidth)
-                        ]
+
+encodeRow : (a -> b) -> List a -> Int -> List b -> ( Int, List b )
+encodeRow f items i acc =
+    case items of
+        px :: rest ->
+            let
+                newI =
+                    i + 1
+
+                newAcc =
+                    f px :: acc
             in
-            ( height + 1, withPaddedBytes :: acc )
-        )
-        ( 0, [] )
-        data
-        |> Tuple.pair width
-    )
-        |> (\( w, ( h, body_ ) ) ->
-                let
-                    body =
-                        applyIf (order == RightUp || order == LeftUp) List.reverse body_ |> E.sequence |> E.encode
+            encodeRow f rest newI newAcc
 
-                    header =
-                        if format == RGBA then
-                            header32 w h (Bytes.width body)
-
-                        else
-                            header16_24 (bitsPerPixel_ format) w h (Bytes.width body)
-                in
-                E.sequence
-                    [ E.sequence header
-                    , E.bytes body
-                    ]
-                    |> E.encode
-           )
-
-
-encodeRow : (Int -> Bool) -> (a -> b) -> List a -> Int -> List b -> ( Int, List b )
-encodeRow limit f items i acc =
-    if not (limit i) then
-        case items of
-            px :: rest ->
-                let
-                    newI =
-                        i + 1
-
-                    newAcc =
-                        f px :: acc
-                in
-                encodeRow limit f rest newI newAcc
-
-            _ ->
-                ( i, acc )
-
-    else
-        ( i, acc )
-
-
-applyIf : Bool -> (a -> a) -> a -> a
-applyIf bool f a =
-    if bool then
-        f a
-
-    else
-        a
+        _ ->
+            ( i, acc )
 
 
 bytesPerPixel_ : PixelFormat -> number
@@ -244,116 +240,122 @@ header2_4_8 =
     []
 
 
-header16_24 : Int -> Int -> Int -> Int -> List Encoder
-header16_24 bitsPerPixel w h dataSize =
-    [ {- BMP Header -}
-      -- "BM" -|- ID field ( 42h, 4Dh )
-      --   [ 0x42, 0x4D ] |> List.map unsignedInt8
-      unsignedInt16 BE 0x424D
-
-    --   70 bytes (54+16) -|- Size of the BMP file
-    -- , [ 0x46, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE (54 + dataSize)
-
-    -- -- Unused -|- Application specific
-    -- , [ 0x00, 0x00 ] |> List.map
-    -- -- Unused -|- Application specific
-    -- , [ 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE 0
-
-    -- 54 bytes (14+40) -|- Offset where the pixel array (bitmap data) can be found
-    -- , [ 0x36, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE (14 + 40)
-
-    {- DIB Header -}
-    --40 bytes -|- Number of bytes in the DIB header (from this point)
-    -- , [ 0x28, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE 40
-
-    -- 2 pixels (left to right order) -|- Width of the bitmap in pixels
-    -- , [ 0x02, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE w
-
-    -- 2 pixels (bottom to top order) -|- Height of the bitmap in pixels. Positive for bottom to top pixel order.
-    -- , [ 0x02, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE h
-
-    --1 plane -|-  Number of color planes being used
-    -- , [ 0x01, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt16 LE 1
-
-    -- 24 bits -|- Number of bits per pixel
-    -- , [ 0x18, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt16 LE bitsPerPixel
-
-    -- 0 -|- BI_RGB, no pixel array compression used
-    -- , [ 0x00, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE 0
-
-    -- 16 bytes -|- Size of the raw bitmap data (including padding)
-    -- , [ 0x10, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE dataSize
-
-    -- 2835 pixels/metre horizontal  | Print resolution of the image, 72 DPI × 39.3701 inches per metre yields 2834.6472
-    -- , [ 0x13, 0x0B, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE 2835
-
-    -- 2835 pixels/metre vertical
-    -- , [ 0x13, 0x0B, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE 2835
-
-    -- 0 colors -|- Number of colors in the palette
-    -- , [ 0x00, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE 0
-
-    -- 0 important colors -|- 0 means all colors are important
-    -- , [ 0x00, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
-    , unsignedInt32 LE 0
-    ]
+header16_24 : Int -> Int -> Int -> Int -> List Encoder -> List Encoder
+header16_24 bitsPerPixel w h dataSize accum =
+    {- BMP Header -}
+    -- "BM" -|- ID field ( 42h, 4Dh )
+    --   [ 0x42, 0x4D ] |> List.map unsignedInt8
+    unsignedInt16 BE 0x424D
+        --   70 bytes (54+16) -|- Size of the BMP file
+        -- , [ 0x46, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE (54 + dataSize)
+        -- -- Unused -|- Application specific
+        -- , [ 0x00, 0x00 ] |> List.map
+        -- -- Unused -|- Application specific
+        -- , [ 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE 0
+        -- 54 bytes (14+40) -|- Offset where the pixel array (bitmap data) can be found
+        -- , [ 0x36, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE (14 + 40)
+        {- DIB Header -}
+        --40 bytes -|- Number of bytes in the DIB header (from this point)
+        -- , [ 0x28, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE 40
+        -- 2 pixels (left to right order) -|- Width of the bitmap in pixels
+        -- , [ 0x02, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE w
+        -- 2 pixels (bottom to top order) -|- Height of the bitmap in pixels. Positive for bottom to top pixel order.
+        -- , [ 0x02, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE h
+        --1 plane -|-  Number of color planes being used
+        -- , [ 0x01, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt16 LE 1
+        -- 24 bits -|- Number of bits per pixel
+        -- , [ 0x18, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt16 LE bitsPerPixel
+        -- 0 -|- BI_RGB, no pixel array compression used
+        -- , [ 0x00, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE 0
+        -- 16 bytes -|- Size of the raw bitmap data (including padding)
+        -- , [ 0x10, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE dataSize
+        -- 2835 pixels/metre horizontal  | Print resolution of the image, 72 DPI × 39.3701 inches per metre yields 2834.6472
+        -- , [ 0x13, 0x0B, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE 2835
+        -- 2835 pixels/metre vertical
+        -- , [ 0x13, 0x0B, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE 2835
+        -- 0 colors -|- Number of colors in the palette
+        -- , [ 0x00, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE 0
+        -- 0 important colors -|- 0 means all colors are important
+        -- , [ 0x00, 0x00, 0x00, 0x00 ] |> List.map unsignedInt8
+        :: unsignedInt32 LE 0
+        :: accum
 
 
-header32 : Int -> Int -> Int -> List Encoder
-header32 w h dataSize =
-    [ {- BMP Header -}
-      unsignedInt16 BE 0x424D -- "BM"    ID field (42h, 4Dh)
-    , unsignedInt32 LE (122 + dataSize) --  Size of the BMP file
-    , unsignedInt32 LE 0 --Unused - Application specific
-    , unsignedInt32 LE 122 --122 bytes (14+108) Offset where the pixel array (bitmap data) can be found
-
-    {- DIB Header -}
-    , unsignedInt32 LE 108 -- 108 bytes    Number of bytes in the DIB header (from this point)
-    , unsignedInt32 LE w -- Width of the bitmap in pixels
-    , unsignedInt32 LE h -- Height of the bitmap in pixels
-    , unsignedInt16 LE 1 --  Number of color planes being used
-    , unsignedInt16 LE 32 -- Number of bits per pixel
-    , unsignedInt32 LE 3 -- BI_BITFIELDS, no pixel array compression used
-    , unsignedInt32 LE dataSize -- Size of the raw bitmap data (including padding)
-    , unsignedInt32 LE 2835 -- 2835 pixels/metre horizontal
-    , unsignedInt32 LE 2835 -- 2835 pixels/metre vertical
-    , unsignedInt32 LE 0 -- Number of colors in the palette
-    , unsignedInt32 LE 0 --important colors (0 means all colors are important)
-    , unsignedInt32 LE 0xFF000000 --00FF0000 in big-endian Red channel bit mask (valid because BI_BITFIELDS is specified)
-    , unsignedInt32 LE 0x00FF0000 --0000FF00 in big-endian    Green channel bit mask (valid because BI_BITFIELDS is specified)
-    , unsignedInt32 LE 0xFF00 --000000FF in big-endian    Blue channel bit mask (valid because BI_BITFIELDS is specified)
-    , unsignedInt32 LE 0xFF --FF000000 in big-endian    Alpha channel bit mask
-    , unsignedInt32 LE 0x206E6957 --   little-endian "Win "    LCS_WINDOWS_COLOR_SPACE
-
-    --CIEXYZTRIPLE Color Space endpoints    Unused for LCS "Win " or "sRGB"
-    , unsignedInt32 LE 0
-    , unsignedInt32 LE 0
-    , unsignedInt32 LE 0
-    , unsignedInt32 LE 0
-    , unsignedInt32 LE 0
-    , unsignedInt32 LE 0
-    , unsignedInt32 LE 0
-    , unsignedInt32 LE 0
-    , unsignedInt32 LE 0
-
-    -----------
-    , unsignedInt32 LE 0 --0 Red Gamma    Unused for LCS "Win " or "sRGB"
-    , unsignedInt32 LE 0 --0 Green Gamma    Unused for LCS "Win " or "sRGB"
-    , unsignedInt32 LE 0 --0 Blue Gamma    Unused for LCS "Win " or "sRGB"
-    ]
+header32 : Int -> Int -> Int -> List Encoder -> List Encoder
+header32 w h dataSize accum =
+    {- BMP Header -}
+    unsignedInt16 BE 0x424D
+        -- "BM"    ID field (42h, 4Dh)
+        :: unsignedInt32 LE (122 + dataSize)
+        --  Size of the BMP file
+        :: unsignedInt32 LE 0
+        --Unused - Application specific
+        :: unsignedInt32 LE 122
+        --122 bytes (14+108) Offset where the pixel array (bitmap data) can be found
+        {- DIB Header -}
+        :: unsignedInt32 LE 108
+        -- 108 bytes    Number of bytes in the DIB header (from this point)
+        :: unsignedInt32 LE w
+        -- Width of the bitmap in pixels
+        :: unsignedInt32 LE h
+        -- Height of the bitmap in pixels
+        :: unsignedInt16 LE 1
+        --  Number of color planes being used
+        :: unsignedInt16 LE 32
+        -- Number of bits per pixel
+        :: unsignedInt32 LE 3
+        -- BI_BITFIELDS:: no pixel array compression used
+        :: unsignedInt32 LE dataSize
+        -- Size of the raw bitmap data (including padding)
+        :: unsignedInt32 LE 2835
+        -- 2835 pixels/metre horizontal
+        :: unsignedInt32 LE 2835
+        -- 2835 pixels/metre vertical
+        :: unsignedInt32 LE 0
+        -- Number of colors in the palette
+        :: unsignedInt32 LE 0
+        --important colors (0 means all colors are important)
+        :: unsignedInt32 LE 0xFF000000
+        --00FF0000 in big-endian Red channel bit mask (valid because BI_BITFIELDS is specified)
+        :: unsignedInt32 LE 0x00FF0000
+        --0000FF00 in big-endian    Green channel bit mask (valid because BI_BITFIELDS is specified)
+        :: unsignedInt32 LE 0xFF00
+        --000000FF in big-endian    Blue channel bit mask (valid because BI_BITFIELDS is specified)
+        :: unsignedInt32 LE 0xFF
+        --FF000000 in big-endian    Alpha channel bit mask
+        :: unsignedInt32 LE 0x206E6957
+        --   little-endian "Win "    LCS_WINDOWS_COLOR_SPACE
+        --CIEXYZTRIPLE Color Space endpoints    Unused for LCS "Win " or "sRGB"
+        :: unsignedInt32 LE 0
+        :: unsignedInt32 LE 0
+        :: unsignedInt32 LE 0
+        :: unsignedInt32 LE 0
+        :: unsignedInt32 LE 0
+        :: unsignedInt32 LE 0
+        :: unsignedInt32 LE 0
+        :: unsignedInt32 LE 0
+        :: unsignedInt32 LE 0
+        -----------
+        :: unsignedInt32 LE 0
+        --0 Red Gamma    Unused for LCS "Win " or "sRGB"
+        :: unsignedInt32 LE 0
+        --0 Green Gamma    Unused for LCS "Win " or "sRGB"
+        :: unsignedInt32 LE 0
+        --0 Blue Gamma    Unused for LCS "Win " or "sRGB"
+        :: accum
 
 
 decode32 : { a | pixelStart : Int, dataSize : Int, width : Int } -> Decoder Image
